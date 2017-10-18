ae100625938a0117d5e62c13c25c4c3adf5a1b62
Your Name <you@example.com>
2017-10-18 16:13:15 -0700

    added shell-like interface, encrypt and create basic password file

--- letmein.c
+++ letmein.c
@@ -9,7 +9,22 @@
 #include "json/cJSON.c"
 
 #define KEYLEN 16
-#define HEADER_SIZE 20
+#define HEADER_SIZE 17
+#define NWORDS 5
+
+/* Splits the given null-terminated string str[] by the spaces.
+ * The resulting words are stored in splits[]. Returns the # of words */
+int splitstring(char *str, char *splits[], int len)
+{
+	int i = 1;
+	if ((splits[0] = strtok(str, " \n\t")) == NULL)
+		return 0;
+	//check i < NWORDS to prevent out-of-bounds (*splits[NWORDS])
+	while (((splits[i] = strtok(NULL, " \n\t")) != NULL) && (i < len)) {
+		i++;
+	}
+	return i;
+}
 
 /* store len bytes of entropy in arr */
 void get_random_bytes(unsigned char *arr, int len)
@@ -22,6 +37,7 @@ void get_random_bytes(unsigned char *arr, int len)
 	close(fd);
 }
 
+/* read initialization vector into string arr */
 void get_iv(unsigned char *arr, int len)
 {
 	FILE *ivfile = fopen("iv.txt", "r");
@@ -37,15 +53,17 @@ void key_from_password(char *pass, int passlen, unsigned char *key)
 
 // do_encrypt -> 1 = encryption, 0 = decryption
 // writes the decrypted contents to *out
-int do_crypt(FILE *in, FILE *outfile, char *out, int do_encrypt,
-	     unsigned char *key)
+int do_crypt(FILE *infile, char *in, FILE *outfile, char *out,
+	     int do_encrypt, unsigned char *key)
 {
 	/* Allow enough space in output buffer for additional block */
-	unsigned char inbuf[1024], outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
-	int inlen, outlen;
+	unsigned char  inbuf[1024];
+	unsigned char  outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
+	int 	       inlen;
+	int	       outlen;
 	EVP_CIPHER_CTX ctx;
 
-	unsigned char iv[KEYLEN];
+	unsigned char  iv[KEYLEN];
 
 	if (do_encrypt == 0)
 		get_iv(iv, sizeof(iv));
@@ -64,9 +82,18 @@ int do_crypt(FILE *in, FILE *outfile, char *out, int do_encrypt,
 	/* Now we can set key and IV */
 	EVP_CipherInit_ex(&ctx, NULL, NULL, key, iv, do_encrypt);
 
+	int offset = 0;
+
 	for(;;) 
 	{
-		inlen = fread(inbuf, 1, 1024, in);
+		if (do_encrypt == 1) {
+			inlen = strnlen(in + offset, 1024);
+			memcpy(inbuf, in + offset, inlen);
+			offset += 1024;
+		} else {
+			inlen = fread(inbuf, 1, 1024, infile);
+		}
+
 		if(inlen <= 0)
 			break;
 		if(!EVP_CipherUpdate(&ctx, outbuf, &outlen, inbuf, inlen))
@@ -75,6 +102,7 @@ int do_crypt(FILE *in, FILE *outfile, char *out, int do_encrypt,
 			EVP_CIPHER_CTX_cleanup(&ctx);
 			return 0;
 		}
+		// if encrypting write to file, else write to string out
 		if (do_encrypt == 1)
 			fwrite(outbuf, 1, outlen, outfile);
 		else
@@ -101,26 +129,10 @@ int do_crypt(FILE *in, FILE *outfile, char *out, int do_encrypt,
 void print_usage()
 {
 	printf("Usage:\n" \
-		"\t./letmein [-e/-d] infile outfile\n"
+		"\t./letmein\n"
 		"-e: encrypt\n"
 		"-d: decrypt\n");
 }
-
-/*void parse_args(char *argv[], FILE *in, FILE *out)*/
-/*{*/
-	/*char *opt = argv[1];*/
-	/*char *infile = argv[2];*/
-	/*char *outfile = argv[3];*/
-
-	/*in = fopen(infile, "r");*/
-	/*out = fopen(outfile, "wb");*/
-	
-	/*if (!strcmp(opt, "-e")) {*/
-		/*do_crypt(in, out, 1, key);*/
-	/*} else if (!strcmp(opt, "-d")) {*/
-		/*do_crypt(in, out, 0, key);*/
-	/*}*/
-/*}*/
 
 void read_passwd(char *pass, int len)
 {
@@ -128,6 +140,8 @@ void read_passwd(char *pass, int len)
 	fgets(pass, len, stdin);
 }
 
+/* Reads entire file fp and returns the contents in a heap-allocated
+ * char pointer */
 char *read_file(FILE *fp)
 {
 	char *buf;
@@ -159,13 +173,83 @@ char *read_file(FILE *fp)
 	return buf;
 }
 
-// add iv (initialization vector), random data
+/* Creates a password storage file filename, consisting of the
+ * hash (in hex) and the json data */
+void create_pass_file(char *filename)
+{
+	char data_json[1024];
+
+	char pass[40];
+	unsigned char key[16];
+	read_passwd(pass, sizeof(pass));
+	key_from_password(pass, strlen(pass), key);
+
+	// convert key to hex
+	unsigned char key_hex[32];
+	for (int i = 0; i < 16; i++)
+		sprintf((char*)key_hex + i*2, "%02x", key[i]);
+
+	// copy key to beginning of data_json
+	memcpy(data_json, key_hex, sizeof(key_hex));
+	// add newline after the key
+	data_json[sizeof(key_hex)] = '\n';
+
+	cJSON *root;
+	root = cJSON_CreateObject();
+	cJSON_AddItemToObject(root, "site", cJSON_CreateString("fc.com"));
+	char *json = cJSON_Print(root);
+
+	// copy json string after the 0xKey
+	memcpy(data_json + sizeof(key_hex) + 1, json, strlen(json) + 1);
+	/*printf("%s\n", data_json);*/
+
+	FILE *passfile = fopen(filename, "wb");
+
+	do_crypt(NULL, data_json, passfile, NULL, 1, key);
+	// dont forget to clean up!
+	cJSON_Delete(root);
+	fclose(passfile);
+}
+
+void parse_insert(char *args[], int nstr)
+{
+	char filename[40];
+	sprintf(filename, "%s.letmein", args[0]);
+	create_pass_file(filename);
+}
+
+void parse_arg(char *splits[], int nstr, short *quitshell)
+{
+	if (nstr > 0) {
+		if (!strcmp(splits[0], "q") || !strcmp(splits[0], "quit")) {
+			*quitshell = 1;
+			exit(1);
+		} else if (!strcmp(splits[0], "help")) {
+			//TODO: parse help
+			printf("dunno man, read the manual\n");
+		} else if (!strcmp(splits[0], "insert")) {
+			parse_insert(splits + 1, nstr - 1);
+		} else {
+			printf("Unknown command. Type help for a list of "
+			       "commands.\n");
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	/*if (argc < 4) {*/
-		/*print_usage();*/
-		/*exit(1);*/
-	/*}*/
+	short quit = 0;
+	char  buffer[64];
+	char  *splits[NWORDS];
+	int   nstr;
+
+	while (!quit) {
+		printf("> ");
+		fgets(buffer, sizeof(buffer), stdin);
+
+		nstr = splitstring(buffer, splits, NWORDS);
+		parse_arg(splits, nstr, &quit);
+	}
 
 	char pass[40];
 	unsigned char key[16];
@@ -175,7 +259,7 @@ int main(int argc, char *argv[])
 	FILE *passfile = fopen("shadow.letmein", "r");
 	char buf[4096];
 
-	do_crypt(passfile, NULL, buf, 0, key);
+	do_crypt(passfile, NULL, NULL, buf, 0, key);
 
 	fclose(passfile);
 
